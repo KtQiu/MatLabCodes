da55e321375b2f66c3e8746b7f7c43da627c24fd
Kt Qiu <kitty666ball@gmail.com>
2017-07-25 21:58:29 +0800

    Add files via upload

---
+++ cnn_exercise/cnnPool.m
@@ -0,0 +1,40 @@
+function pooledFeatures = cnnPool(poolDim, convolvedFeatures)
+%cnnPool Pools the given convolved features
+%
+% Parameters:
+%  poolDim - dimension of pooling region
+%  convolvedFeatures - convolved features to pool (as given by cnnConvolve)
+%                      convolvedFeatures(imageRow, imageCol, featureNum, imageNum)
+%
+% Returns:
+%  pooledFeatures - matrix of pooled features in the form
+%                   pooledFeatures(poolRow, poolCol, featureNum, imageNum)
+%     
+
+numImages = size(convolvedFeatures, 4);
+numFilters = size(convolvedFeatures, 3);
+convolvedDim = size(convolvedFeatures, 1);
+
+pooledFeatures = zeros(convolvedDim / poolDim, ...
+        convolvedDim / poolDim, numFilters, numImages);
+
+% Instructions:
+%   Now pool the convolved features in regions of poolDim x poolDim,
+%   to obtain the 
+%   (convolvedDim/poolDim) x (convolvedDim/poolDim) x numFeatures x numImages 
+%   matrix pooledFeatures, such that
+%   pooledFeatures(poolRow, poolCol, featureNum, imageNum) is the 
+%   value of the featureNum feature for the imageNum image pooled over the
+%   corresponding (poolRow, poolCol) pooling region. 
+%   
+%   Use mean pooling here.
+for k = 1:poolDim:convolvedDim
+    for u = 1:poolDim:convolvedDim
+        pooledFeatures(((k-1)/poolDim)+1,((u-1)/poolDim)+1) = mean(mean(convolvedFeatures(k:k+poolDim-1,u:u+poolDim-1)));
+    end
+end
+
+%%% YOUR CODE HERE %%%
+
+end
+
